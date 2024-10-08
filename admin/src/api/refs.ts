a20cb68fdf8d84781bb0b120defbbb951d90e691
Garnik Simonyan <gsimonyan@hotmail.com>
2024-10-08 19:14:22 +0400

    Rename refresh endpoint

--- admin/src/api/refs.ts
+++ admin/src/api/refs.ts
@@ -2,7 +2,7 @@ export const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api`;
 
 export enum REFS {
   AUTH = 'auth/login',
-  REFRESH_TOKEN = 'auth/exchange-refresh-token',
+  REFRESH_TOKEN = 'auth/refresh-token',
   EXPERTS = 'experts',
   LANGUAGES = 'languages',
   LOCATIONS = 'locations',
