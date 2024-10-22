df01ae85cde309315315dbcf818c01950d97d22e
Garnik Simonyan <gsimonyan@hotmail.com>
2024-10-22 13:19:37 +0400

    Redesign footer

--- site/src/components/footer/index.tsx
+++ site/src/components/footer/index.tsx
@@ -1,4 +1,5 @@
-import logo from '@/assets/images/logo-light.svg';
+import darkLogo from '@/assets/images/logo-light.svg';
+import sphereLogo from '@/assets/images/sphere-logo.svg';
 
 import { CustomNavLink } from '../custom-nav-link';
 import { PATHS } from '../routes/paths';
@@ -11,15 +12,28 @@ export const Footer = () => {
   return (
     <footer className={styles.root}>
       <div className={styles.footer}>
-        <img src={logo} alt="logo" className={styles.logo} />
-        <span>
-          © 2005-{getCurrentYear()} Copyright&nbsp;
-          <a className={styles.sphereLink} href="https://www.sphereinc.com">
-            Sphere Software, LLC
-          </a>
-          . All Rights Reserved.
-        </span>
-        <CustomNavLink to={PATHS.PRIVACY_POLICY}>Privacy Policy</CustomNavLink>
+        <div className={styles.firstRow}>
+          <img src={darkLogo} alt="logo" className={styles.logo} />
+
+          <div className={styles.mainSiteInfo}>
+            <span>Visit our main site</span>
+            <img src={sphereLogo} alt="logo" className={styles.logo} />
+          </div>
+
+          <div className={styles.externalLinks}>
+            <CustomNavLink to={PATHS.PRIVACY_POLICY}>Terms and Conditions</CustomNavLink>
+            <CustomNavLink to={PATHS.PRIVACY_POLICY}>Privacy Policy</CustomNavLink>
+          </div>
+        </div>
+        <div className={styles.secondRow}>
+          <span>
+            © 2005-{getCurrentYear()} Copyright&nbsp;
+            <a className={styles.sphereLink} href="https://www.sphereinc.com">
+              Sphere Software, LLC
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
       </div>
     </footer>
   );
